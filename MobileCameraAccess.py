1b740cd8d547e261b9533f474ea4826253c1125c
Ayush Puri <38379529+puriayush29@users.noreply.github.com>
2023-08-30 19:07:41 +0530

    Update MobileCameraAccess.py

--- MobileCameraAccess.py
+++ MobileCameraAccess.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 while True:
-    images=requests.get("http://192.168.29.126:8080/shot.jpg") #MoM Server IP Address
+    images=requests.get("http://IPAddress:Port/shot.jpg") #MoM Server IP Address
     #images=requests.get("http://100.91.236.144:8080/shot.jpg") #Dad Server IP Address
     video=np.array(bytearray(images.content),dtype=np.uint8)
     render = cv2.imdecode(video,-1)
