9e20a6d768a7fdf32776527869e9a7f9b85f34fc
Ayush Puri <38379529+puriayush29@users.noreply.github.com>
2023-08-30 19:07:11 +0530

    Create MobileCameraAccess.py

---
+++ MobileCameraAccess.py
@@ -0,0 +1,12 @@
+import requests
+import numpy as np
+import cv2
+
+while True:
+    images=requests.get("http://192.168.29.126:8080/shot.jpg") #MoM Server IP Address
+    #images=requests.get("http://100.91.236.144:8080/shot.jpg") #Dad Server IP Address
+    video=np.array(bytearray(images.content),dtype=np.uint8)
+    render = cv2.imdecode(video,-1)
+    cv2.imshow('frame',render)
+    if(cv2.waitKey(1) and 0xFF==ord('q')):
+        break
